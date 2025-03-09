6c7f8c2e2495637f980f1924b169a37896b10c05
cawtoz <cawtoz.dev@gmail.com>
2025-03-09 15:24:06 -0500

    Add development entry point and example event handler

---
+++ src/main/kotlin/com/github/cawtoz/minestom/dev/Main.kt
@@ -0,0 +1,27 @@
+package com.github.cawtoz.minestom.dev
+
+import com.github.cawtoz.minestom.core.event.EventBus
+import net.minestom.server.MinecraftServer
+import net.minestom.server.coordinate.Pos
+import net.minestom.server.event.player.AsyncPlayerConfigurationEvent
+import net.minestom.server.instance.block.Block
+
+fun main() {
+
+    val minecraftServer = MinecraftServer.init()
+    val instanceManager = MinecraftServer.getInstanceManager()
+    val instanceContainer = instanceManager.createInstanceContainer()
+
+    instanceContainer.setGenerator { it.modifier().setBlock(0, 99, 0, Block.BEDROCK) }
+
+    EventBus.registerEvent<AsyncPlayerConfigurationEvent> { event ->
+        event.spawningInstance = instanceContainer
+        val player = event.player
+        player.respawnPoint = Pos(0.5, 100.0, 0.5)
+    }
+
+    EventBus.registerHandler(ExampleEventHandler())
+
+    minecraftServer.start("0.0.0.0", 25565)
+
+}
