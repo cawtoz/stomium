7cc1b4d7fa21d95f8e682f6837512fd7c799fba4
cawtoz <cawtoz.dev@gmail.com>
2025-03-10 10:53:35 -0500

    Updated example events

---
+++ src/main/kotlin/com/github/cawtoz/minestom/dev/ExampleEvents.kt
@@ -0,0 +1,74 @@
+package com.github.cawtoz.minestom.dev
+
+import com.github.cawtoz.minestom.core.event.EventHandler
+import com.github.cawtoz.minestom.core.event.EventManager
+import net.minestom.server.entity.Player
+import net.minestom.server.event.entity.EntityAttackEvent
+import net.minestom.server.event.player.PlayerChatEvent
+import net.minestom.server.event.player.PlayerDeathEvent
+import net.minestom.server.event.player.PlayerMoveEvent
+import net.minestom.server.utils.time.TimeUnit
+
+/**
+ * Registers global events using EventManager.
+ * If you want to register an event in a specific node instead of globally,
+ * provide a `nodeName` as the first argument.
+ */
+fun exampleEvents() {
+
+    // Cancels messages containing "badword".
+    EventManager.onEvent<PlayerChatEvent>/*("nodeName")*/ { event ->
+        if (event.rawMessage.contains("badword")) {
+            event.player.sendMessage("Don't use bad words!")
+            event.isCancelled = true
+        }
+    }
+
+    // Player death handling with conditions and expiration.
+    EventManager.onBuilderEvent<PlayerDeathEvent>/*("nodeName")*/ {
+        filter { event -> event.player.username != "cawtoz" } // Ignore if player is "cawtoz".
+        ignoreCancelled(false) // Execute even if canceled.
+        expireCount(10) // Expires after 10 executions.
+        expireWhen { event -> event.player.health > 10.0 } // Expires if HP > 10.
+
+        handler { event ->
+            event.player.sendMessage("You died! Respawning in 5 seconds...")
+            event.player.scheduler().buildTask { event.player.respawn() }
+                .delay(5, TimeUnit.SECOND).schedule()
+        }
+    }
+
+}
+
+/**
+ * Groups events using EventHandler.
+ * If `nodeName` is provided, the events will only be triggered inside that node.
+ */
+class ExampleEventHandler : EventHandler({
+
+    // Notifies a player when hit by another player.
+    onEvent<EntityAttackEvent> { event ->
+
+        val target = event.target
+        val entity = event.entity
+
+        if (target is Player && entity is Player) {
+            target.sendMessage("You were hit by ${entity.username}!")
+        }
+
+    }
+
+    // Prevents players from falling out of the world.
+    onBuilderEvent<PlayerMoveEvent> {
+
+        filter { event -> event.newPosition.y < 0 } // Trigger only if below Y=0.
+        expireCount(5) // Expires after 5 executions.
+
+        handler { event ->
+            event.player.teleport(event.player.respawnPoint)
+            event.player.sendMessage("You can't fall out of the world!")
+        }
+
+    }
+
+}/*, "nodeName"*/)
--- src/main/kotlin/com/github/cawtoz/minestom/dev/Main.kt
+++ src/main/kotlin/com/github/cawtoz/minestom/dev/Main.kt
@@ -1,6 +1,6 @@
 package com.github.cawtoz.minestom.dev
 
-import com.github.cawtoz.minestom.core.event.EventBus
+import com.github.cawtoz.minestom.core.event.EventManager
 import net.minestom.server.MinecraftServer
 import net.minestom.server.coordinate.Pos
 import net.minestom.server.event.player.AsyncPlayerConfigurationEvent
@@ -14,13 +14,14 @@ fun main() {
 
     instanceContainer.setGenerator { it.modifier().setBlock(0, 99, 0, Block.BEDROCK) }
 
-    EventBus.registerEvent<AsyncPlayerConfigurationEvent> { event ->
+    EventManager.onEvent<AsyncPlayerConfigurationEvent> { event ->
         event.spawningInstance = instanceContainer
         val player = event.player
         player.respawnPoint = Pos(0.5, 100.0, 0.5)
     }
 
-    EventBus.registerHandler(ExampleEventHandler())
+    exampleEvents()
+    EventManager.registerHandler(ExampleEventHandler())
 
     minecraftServer.start("0.0.0.0", 25565)
 
